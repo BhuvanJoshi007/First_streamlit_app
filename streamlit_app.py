cfa8e39d937d2d2fa30ec6781d58f3fb6d0e9d80
Bhuvan Joshi <75200183+BhuvanJoshi007@users.noreply.github.com>
2023-11-14 18:16:26 +0530

    Update streamlit_app.py
    Just to check an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new edited healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('This is the first meal on Menu')
+streamlit.text('This is the second meal on menu')
+streamit.text('And this is the last')
