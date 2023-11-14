d4c3f3e7554a60bd9867c921b5c850f8bfdc7594
Bhuvan Joshi <75200183+BhuvanJoshi007@users.noreply.github.com>
2023-11-14 18:43:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents new edited healthy dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('This is the first meal on Menu')
-streamlit.text('This is the second meal on menu')
-streamlit.text('And this is the last')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
