25eda8984e956578ab33c751e18bdacc0f5fc842
Bhuvan Joshi <75200183+BhuvanJoshi007@users.noreply.github.com>
2023-11-26 09:42:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 # Add A Second Text Entry Box
 fruit_choice_2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice_2)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
