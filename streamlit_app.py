e646e1d72a6d7eb7a29f6739bf969d0b510a97db
Bhuvan Joshi <75200183+BhuvanJoshi007@users.noreply.github.com>
2023-11-26 10:49:06 +0530

    Update streamlit_app.py
    Add a STOP Command to Focus Our Attention

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new edited healthy dinner')
 
@@ -22,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # To display fruityvice API response
-import requests
+# import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -34,8 +37,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # This will display the dataframe
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
 
+# do not run anything beyond this line
+streamlit.stop()
+
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
